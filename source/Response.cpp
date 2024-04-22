a167b71a2d2604db385a2cc375adfd6378cdb399
Vincent Ockers <vincentbockers@gmail.com>
2024-04-22 21:31:55 +0200

    moved cgi to source

---
+++ source/Response.cpp
@@ -0,0 +1,10 @@
+#include "Response.hpp"
+
+Response::Response(const HttpRequest &request)
+{
+    // if (request path does not exist)
+    // {
+    //     _code = StatusCode::NotFound;
+    //     return;
+    // }
+}
