db1f4791e4697aa2b513636c3c632be195e9a96f
Vincent Ockers <vincentbockers@gmail.com>
2024-04-22 21:31:55 +0200

    moved cgi to source

--- include/Response.hpp
+++ include/Response.hpp
@@ -4,11 +4,14 @@
 #include <map>
 
 #include "StatusCode.hpp"
+#include "HttpRequest.hpp"
 
 using HeaderMap = std::map<std::string, std::string>;
 
 class Response
 {
+public:
+    Response(const HttpRequest &request);
 private:
     StatusCode _code;
     HeaderMap _headers;
