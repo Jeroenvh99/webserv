5edda8a91d0e5e71057deb3ab6a06d4fc0a78d1c
Vincent Ockers <vincentbockers@gmail.com>
2024-04-30 13:20:11 +0200

    Add basic response

--- source/Response.cpp
+++ source/Response.cpp
@@ -7,4 +7,18 @@ Response::Response(const HttpRequest &request)
     //     _code = StatusCode::NotFound;
     //     return;
     // }
-}
+    _code = StatusCode::OK;
+
+    _body = "Hello World!";
+
+    _response = "HTTP/1.1 ";
+	_response += std::to_string(static_cast<int>(_code)) + " " + _statusMap.at(static_cast<int>(_code)) + "\r\n";
+	_response += "Content-Length: " + std::to_string(_body.size()) + "\r\n";
+	_response += "Content-Type: text/html\r\n\r\n";
+	_response += _body;
+ }
+
+ const std::string &Response::get()
+ {
+    return _response;
+ }
