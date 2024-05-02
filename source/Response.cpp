1b74b5e782a1861dbe414fc2234bfdfd0340ae9b
Vincent Ockers <vincentbockers@gmail.com>
2024-05-02 12:13:30 +0200

    Read from file

--- source/Response.cpp
+++ source/Response.cpp
@@ -1,19 +1,14 @@
 #include "Response.hpp"
 
 #include <filesystem>
+#include <fstream>
 
-Response::Response(const HttpRequest &request)
+Response::Response(const HttpRequest &request) : 
+    _code(StatusCode::OK),
+    _body("")
 {
-    if (!std::filesystem::exists(request.getRequestUri()))
-    {
-        _code = StatusCode::NotFound;
-        _body = "404";
-        return;
-    }
-
-    _code = StatusCode::OK;
-    _body = "Hello World!";
- }
+    readFromFile(request.getRequestUri());
+}
 
  const std::string &Response::get()
  {
@@ -28,3 +23,38 @@ Response::Response(const HttpRequest &request)
 	_response += "Content-Type: text/html\r\n\r\n";
 	_response += _body;
  }
+
+ void Response::readFromFile(const std::string &path)
+ {
+    std::ifstream       file;
+    std::stringstream   buffer;
+
+    
+    if (!std::filesystem::exists(path))
+    {
+        _code = StatusCode::NotFound;
+        _body = "404";
+    }
+    else if (std::filesystem::is_directory(path))
+    {
+        _code = StatusCode::Forbidden;
+        _body = "403";
+    }
+    else
+    {
+        file.open(path);
+        if (!file.is_open())
+        {
+            _code = StatusCode::Forbidden;
+            _body = "403";
+        }
+        else
+        {
+            buffer << file.rdbuf();
+            _body = buffer.str();
+            file.close();
+        }
+    }
+
+    generate();
+}
