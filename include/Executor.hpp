13bdd3438f9a0b827085acde0d7498d408cda954
David Bastings <dp.bastings@posteo.net>
2024-06-14 15:49:27 +0200

    Regular response handling and CGI should probably be combined under a single interface.

---
+++ include/Executor.hpp
@@ -0,0 +1,21 @@
+#ifndef EXECUTOR_HPP
+# define EXECUTOR_HPP
+
+# include "http/Request.hpp"
+# include "http/Response.hpp"
+
+# include <fstream>
+
+class Executor {
+public:
+	enum class State;
+
+	void	execute(http::Request const&);
+
+private:
+	std::fstream	fs;
+}; // class Executor
+
+http::Response&	operator<<(http::Response&, Executor&);
+
+#endif // EXECUTOR_HPP
