ec44754a03cfa660df37dfa044c0f4860e2856d1
David Bastings <dp.bastings@posteo.net>
2024-07-09 15:21:58 +0200

    Restructuring.

--- include/utils.hpp
+++ include/utils.hpp
@@ -37,35 +37,12 @@ namespace utils {
 
 	class IncompleteLineException {};
 
-	template<utils::StringLiteral DELIM>
-	std::istream&
-	getline_core(std::istream& is, std::string& str) {
-		char	c;
-
-		str.clear();
-		while (is.get(c)) {
-			str.push_back(c);
-			if (str.length() >= DELIM.length && str.ends_with(DELIM.value)) {
-				str.erase(str.length() - DELIM.length);
-				break;
-			}
-		}
-		return (is);
-	}
-
-	template<utils::StringLiteral DELIM>
-	std::istream&
-	getline(std::istream& is, std::string& str) {
-		std::istream::iostate	state = is.rdstate();
-		auto					pos = is.tellg();
-
-		getline_core<DELIM>(is, str);
-		if (is.good())
-			return (is);
-		is.clear(state);
-		is.seekg(pos);
-		throw (IncompleteLineException());
-	}
+	template<StringLiteral DELIM>
+	std::istream&	getline(std::istream&, std::string&);
+	template<StringLiteral DELIM>
+	std::istream&	getline_core(std::istream&, std::string&);
 }; // namespace utils
+
+# include "utils.tpp"
 
 #endif // UTILS_HPP
