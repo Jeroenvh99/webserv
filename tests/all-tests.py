f2fb6a54416dd8e6853850e1478456a17e179373
Jeroen Van Halderen <jvan-hal@f1r6s5.codam.nl>
2024-10-01 18:53:43 +0200

    made some more tests

--- tests/all-tests.py
+++ tests/all-tests.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 BASE_URL = "http://localhost:1100"
 
@@ -50,32 +51,45 @@ def test_unallowed_method():
     assert response.status_code == 405
 
 
-# def test_reponse_headers():
-#     response = requests.get(f'{BASE_URL}/')
-#
-#     assert response.headers['Server'] == 'webserv'
-#     assert response.headers['Date']
-#     assert response.headers['Content-Type'] == 'text/html'
-#     assert response.headers['Content-Length'] == '159'
-#     assert response.headers['Last-Modified'] == 'Tue, 27 Aug 2024 17:02:14 GMT'
-    # assert response.headers['Last-Modified']
+def test_reponse_headers():
+    response = requests.get(f'{BASE_URL}/')
 
-# def test_bad_request():
-#     expected_body = '''\
-# <html lang="en-US">
-#     <head>
-#         <meta charset="utf-8" />
-#         <title>400 Bad Request</title>
-#     </head>
-#     <body>
-#         <h1 align="center">
-#             400 Bad Request</h1>
-#         <hr />
-#         <p align="center">webserv</p>
-#     </body>
-# </html>
-# '''
-#
-#     response = requests.post(f'{BASE_URL}/', headers={'Content-Type': 'application/json', 'Content-Length': 'no'}, data='{ "title":"foo","body":"bar","id":1 }')
-#
-#     assert response.status_code == 400
+    assert response.headers['Server'] == 'webserv'
+    assert response.headers['Content-Type'] == 'text/html'
+    assert response.headers['Content-Length'] == '3473'
+    assert response.headers['Last-Modified'] == 'Tue, 27 Aug 2024 17:02:14 GMT'
+    assert response.headers['Connection'] == 'keep-alive'
+
+def test_bad_request():
+    expected_body = '''\
+<html lang="en-US">
+    <head>
+        <meta charset="utf-8" />
+        <title>400 Bad Request</title>
+    </head>
+    <body>
+        <h1 align="center">
+            400 Bad Request</h1>
+        <hr />
+        <p align="center">webserv</p>
+    </body>
+</html>
+'''
+
+    response = requests.post(f'{BASE_URL}/', headers={'Content-Type': 'application/json', 'Content-Length': 'no'}, data='{ "title":"foo","body":"bar","id":1 }')
+
+    assert response.status_code == 400
+	assert response.text == expected_body
+
+def test_cgi():
+    response = requests.get(f'{BASE_URL}/cgi/env.py')
+    
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'text/html'
+
+def test_post():
+	files = {'upload_file': open('/home/jvan-hal/Desktop/webserv/www/default/cgi/deleterandom.py','rb')}
+    response = requests.post(f'{BASE_URL}/upload.txt', files=files)
+
+    assert response.status_code == 201
+	assert os.path.exists('/home/jvan-hal/Desktop/webserv/www/default/upload.txt')
