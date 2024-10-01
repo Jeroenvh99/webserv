6d98aa86c000b915864f7ba6fbf74b60cfdd3f8b
Jeroen Van Halderen <jvan-hal@f1r6s5.codam.nl>
2024-10-01 16:30:13 +0200

    fixed the html formatting

--- www/default/cgi/deleterandom.py
+++ www/default/cgi/deleterandom.py
@@ -3,12 +3,25 @@
 import os
 import random
 
-print('delete a random file from the tests folder')
-testfiles = []
-for root, dirs, files in os.walk("/home/jvan-hal/Desktop/webserv/tests"):
-	for file in files:
-		testfiles.append(os.path.join(root, file))
-print("Files in the tests folder")
-print(testfiles)
-deleteindex = random.randint(0, len(testfiles))
-os.remove(testfiles[deleteindex])
+def body:
+	body = "<!DOCTYPE html>"
+	body += "<html>"
+	body += "<head><title>Random delete</title></head>"
+	body += "<body>"
+	body += "<h1>Delete a random file</h1>"
+	body += "<p>delete a random file from the tests folder</p>"
+	testfiles = []
+	for root, dirs, files in os.walk("/home/jvan-hal/Desktop/webserv/tests"):
+		for file in files:
+			testfiles.append(os.path.join(root, file))
+	deleteindex = random.randint(0, len(testfiles))
+	os.remove(testfiles[deleteindex])
+	body += "</body>"
+	body += "</html>"
+	return body
+
+payload = body()
+print("Content-Type: text/html")
+print("Content-Length:", len(payload))
+print("")
+print(payload)
