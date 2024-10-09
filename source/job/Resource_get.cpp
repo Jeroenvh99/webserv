46398be6fd991caef69783b888a215c3e4b2d5dd
David Bastings <dp.bastings@posteo.net>
2024-10-09 22:18:40 +0200

    201 Created responses are now properly issued.

--- source/job/Resource_get.cpp
+++ source/job/Resource_get.cpp
@@ -1,6 +1,10 @@
 #include "job/Resource.hpp"
 
+#include "mime.hpp"
+
 using job::Resource;
+
+static std::string	make_headers(stdfs::path const&);
 
 http::Status
 Resource::_get(route::Location const& loc) {
@@ -18,7 +22,7 @@ Resource::_get_file(stdfs::path const& pt) {
 	if (!_ifs.is_open())
 		return (http::Status::not_found);
 	_iss.clear();
-	_iss.str(make_headers_get(pt));
+	_iss.str(make_headers(pt));
 	return (http::Status::ok);
 }
 
@@ -29,4 +33,15 @@ Resource::_get_directory(route::Location const& loc) {
 	if (loc.lists_directory())
 		return (_open_builtin(_make_directory_list(loc.to())), http::Status::ok);
 	return (_get_file(loc.to() / loc.directory_file()));
+}
+
+std::string
+make_headers(stdfs::path const& pt) {
+	std::ostringstream	oss;
+
+	oss << "Connection: keep-alive\r\n"
+		<< "Content-Type: " << mime::get_type(pt) << "\r\n"
+		<< "Content-Length: " << std::filesystem::file_size(pt) << "\r\n";
+	oss << "\r\n";
+	return (oss.str());
 }
--- source/job/Resource_post.cpp
+++ source/job/Resource_post.cpp
@@ -2,6 +2,8 @@
 
 using job::Resource;
 
+static std::string	make_headers_and_body(route::Location const&);
+
 http::Status
 Resource::_post(route::Location const& loc) {
 	stdfs::path const&	pt = loc.to();
@@ -10,8 +12,20 @@ Resource::_post(route::Location const& loc) {
 	if (!_ofs.good())
 		return (http::Status::internal_error);
 	_iss.clear();
-	_iss.str(make_headers_post(loc));
-	_iss.str("there is something new on you server\r\n");
+	_iss.str(make_headers_and_body(loc));
 
 	return (http::Status::created);
+}
+
+static std::string
+make_headers_and_body(route::Location const& loc) {
+	std::ostringstream	oss;
+	std::string			uri = loc.from().c_str();
+
+	oss << "Connection: keep-alive\r\n"
+		<< "Content-Type: text/plain\r\n"
+		<< "Content-Length: " << uri.length() << "\r\n"
+		<< "Location: " << uri << "\r\n\r\n"
+		<< uri;
+	return (oss.str());
 }
